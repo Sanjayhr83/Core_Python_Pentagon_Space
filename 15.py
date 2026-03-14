e18edde251980bd0d1ec9d55584ae661b91991c5
Sanjay <hrsanjay@gmail.com>
2026-03-14 14:18:53 +0530

    Inheritance

--- 15.py
+++ 15.py
@@ -44,14 +44,13 @@ c.display()
 class Person:
     def __init__(self, name):
         self.name = name
-
 class Student(Person):
     def __init__(self, name, age):
         super().__init__(name)
         self.age = age
-
 s = Student("Sanjay", 21)
 print(s.name, s.age)
+
 
 #without follow impletementation of inheritance
 class Passengerplane:
