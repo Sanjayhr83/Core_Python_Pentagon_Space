6ed745050a3065202b2aef14262363b58ccff0f9
Sanjay <hrsanjay@gmail.com>
2026-04-08 22:18:35 +0530

    Exceptional handling

--- 21.py
+++ 21.py
@@ -1,4 +1,10 @@
 #Exception Handling
+"""
+try : Risk code written in this block
+except: To know the error is occurring
+else : it this run when except block not show error
+finally: it is run always end to the execution code block
+"""
 a=int(input("enter a value : "))
 b=int(input("enter a value : "))
 try:
