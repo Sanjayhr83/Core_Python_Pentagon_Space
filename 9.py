071c0c65d2072b9a694e521d251899690a931fa1
Sanjay <hrsanjay@gmail.com>
2026-04-17 11:15:53 +0530

    static method and class method,ethod return multiple values

--- 9.py
+++ 9.py
@@ -96,3 +96,22 @@ d.disp(z)   #33,20,30
 d.disp(y,z) #22,33,30
 d.disp(a=z,b=y,c=x) #33,22,11
 d.disp(c=y) #10,20,22
+
+#Method return multiple values
+class calci:
+    def operation(self,a,b):
+        c1=a+b
+        c2=a-b
+        c3=a*b
+        c4=a/b
+        return c1,c2,c3,c4  #python automatically pack these into a tuple like (c1,c2,c3,c4)
+c1=calci()
+res=c1.operation(1,2)
+print(res)  #(3,-1,2,0.5)
+r1,r2,r3,r4 = c1.operation(3,4) #Now tuple automatically unpacks
+print(r1,r2,r3,r4)  #7 -1 12 0.75
+print(r1)   #7
+print(r2)   #-1
+print(r3)   #12
+print(r4)   #0.75
+
