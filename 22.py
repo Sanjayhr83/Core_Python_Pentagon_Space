14d4e021eec7802cba656c7a2849b86f78d4b550
Sanjay <hrsanjay@gmail.com>
2026-04-30 10:08:39 +0530

    list, nested list,copy methods(shallow,deep),built-in function in list,indexing and slice in list

--- 22.py
+++ 22.py
@@ -21,3 +21,95 @@ while True:
     else:
         break
 print(l)
+
+#nested list
+l=[10,20,30,["IB","MH","BUZZ"],40]
+print(l)
+print(len(l))  #5
+print(l[4])    #40
+print(l[1])    #20
+print(l[3][1]) #MH
+print(l[3][2]) #BUZZ
+
+l=[10,20,30,["IB","MH","BUZZ",["XL","M80","RX"],"OT"],40]
+print(l)
+print(len(l))  #5
+print(l[4])    #40
+print(l[1])    #20 (1D)
+print(l[3][1]) #MH (2D)
+print(l[3][2]) #BUZZ (2D)
+print(l[3][3][2]) #RX (3D)
+
+#copy methods
+"""Shallow Copy : it is affect both object"""
+l0=[10,20,30,40,50]
+l1=l0   #shallow copy
+print(l0)
+print(l1)
+l0[3]=3000  #modify the object
+print(l0)
+print(l1)
+
+"""Deep Copy : it is affect only one object"""
+l2=[10,20,30,40,50]
+l3=l2.copy()
+print(l2)
+print(l3)
+del l2[2]
+print(l2)
+print(l3)
+
+#nested list
+l=[10,20,["MH","IB"],40]
+l1=l.copy()
+print(l)
+print(l1)
+l[2][1]="BUZZ"
+print(l)
+print(l1)
+
+#achieving deep copy in nested list
+import copy
+l=[10,20,["MH","IB"],40]
+l1=copy.deepcopy(l) #deep copy in nested list
+print(l)
+print(l1)
+l[2][1]="BUZZ"
+print(l)
+print(l1)
+
+#built-in functions in list (len,min,max,type,all,any,sorted)
+l=[10,20,30,40,50]
+print(l)
+print(len(l))
+print(min(l))
+print(max(l))
+print(type(l))
+
+l1=[1,0,1,2]
+print(l1)
+print(all(l1))
+print(any(l1))
+
+l2=[0,0,0,0]
+print(l2)
+print(all(l2))
+print(any(l2))
+
+l3=[0,0,1,0]
+print(l3)
+print(all(l3))
+print(any(l3))
+
+l4=[1,20,"pentagon",30]
+print(l4)
+print(all(l4))
+print(any(l4))
+
+l5=["rahul","rama"]
+print(l5)
+print(all(l5))
+print(any(l5))
+
+l6=[89,78,54,90,74,23]
+print(sorted(l6))
