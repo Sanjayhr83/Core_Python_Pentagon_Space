5a5b78b1a6fc3e3eb0d77373210d3608bc316220
Sanjay <hrsanjay@gmail.com>
2026-05-29 11:40:04 +0530

    File Handling in python

--- 26.py
+++ 26.py
@@ -18,22 +18,52 @@
 #     #ptr.close() # it is open the file 5 times and close the file 5 time
 # ptr.close() # it is open the once and fill the 5 name and close after the filling 5 names
 
+#read()
 # ptr1=open("sanju.txt","r")
 # data=ptr1.read()
 # print(data)
 # ptr1.close()
 
+#read(bytes)
 # ptr2=open("sanju.txt","r")
 # data=ptr2.read(8)
 # print(data)
 # ptr2.close()
 
+#readline()
 # ptr3=open("sanju.txt","r")
 # data=ptr3.readline()
 # print(data)
 # ptr3.close()
 
-ptr4=open("sanju.txt","r")
-data=ptr4.readlines()
-print(data)
-ptr4.close()
+#readlines()
+# ptr4=open("sanju.txt","r")
+# data=ptr4.readliness()
+# print(data)
+# ptr4.close()
+
+
+#tell() and seek()
+#tell() : it will give the current position of the cursor
+#seek() : using this seek() method we can move the cursor from one position to another position
+ptr=open("sanju.txt","r")
+pos1=ptr.tell()
+res1=ptr.read(8)
+print(res1) #Cristano
+
+pos2=ptr.tell()
+print(pos2) #8
+
+ptr.seek(8)
+pos3=ptr.tell()
+print(pos3) #8
+
+ptr.seek(0)
+pos4=ptr.tell()
+print(pos4) #0
+
+res2=ptr.read(15)
+print(res2) #CristanoRonaldo
+pos5=ptr.tell()
+print(pos5)
+ptr.close()
