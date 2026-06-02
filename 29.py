944f97a8e70d2c51bbc9f64cd52764099724be11
Sanjay <hrsanjay@gmail.com>
2026-06-02 06:58:23 +0530

    Single-Threading and Multi-Threading

--- 29.py
+++ 29.py
@@ -1,27 +1,27 @@
 # Multi-tasking or Multi-Processing
 """the ability to do a multiple simultaneously is called multi-tasking"""
 
-# Single Therading
-# import time
-# class demo:
-#     def printname(self):
-#         l=["sanjay","rajappa","shashank"]
-#         for i in l:
-#             print(i)
-#             time.sleep(3)
-#     def printnumber(self):
-#         for i in range(10):
-#             print(i)
-#             time.sleep(3)
-#     def add(self):
-#         a=10
-#         b=20
-#         c=a+b
-#         print(f"{a}+{b}={c}")
-# d1=demo()
-# d1.printname()
-# d1.printnumber()
-# d1.add()
+Single Therading
+import time
+class demo:
+    def printname(self):
+        l=["sanjay","rajappa","shashank"]
+        for i in l:
+            print(i)
+            time.sleep(3)
+    def printnumber(self):
+        for i in range(10):
+            print(i)
+            time.sleep(3)
+    def add(self):
+        a=10
+        b=20
+        c=a+b
+        print(f"{a}+{b}={c}")
+d1=demo()
+d1.printname()
+d1.printnumber()
+d1.add()
 
 #Multi-Threading
 import time
