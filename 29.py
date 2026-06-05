66822e56f3242f0906e9858ad68517bebf1f4d4f
Sanjay <hrsanjay@gmail.com>
2026-06-05 14:17:28 +0530

    Single Thread and Multi-Thread

--- 29.py
+++ 29.py
@@ -23,6 +23,34 @@ d1.printnumber()
 d1.add()
 
 
+#Multi-Threading
+import time
+from threading import Thread
+class Task(Thread):
+    def run(self):
+        l = ["sanjay", "rajappa", "shashank"]
+        for i in l:
+            print(i)
+            time.sleep(3)
+class Task2(Thread):
+    def run(self):
+        for i in range(10):
+            print(i)
+            time.sleep(3)
+class Task3(Thread):
+    def run(self):
+        a=10
+        b=20
+        c=a+b
+        print(f"{a}+{b}={c}")
+t1=Task()
+t2=Task2()
+t3=Task3()
+t1.start()
+t2.start()
+t3.start()
+
+import time
 from threading import Thread
 class Data(Thread):
     def run(self):
@@ -56,32 +84,3 @@ f1.start()
 d1.join()
 c1.join()
 f1.join()
-
-#Multi-Threading
-import time
-from threading import Thread
-class Task(Thread):
-    def run(self):
-        l = ["sanjay", "rajappa", "shashank"]
-        for i in l:
-            print(i)
-            time.sleep(3)
-class Task2(Thread):
-    def run(self):
-        for i in range(10):
-            print(i)
-            time.sleep(3)
-class Task3(Thread):
-    def run(self):
-        a=10
-        b=20
-        c=a+b
-        print(f"{a}+{b}={c}")
-t1=Task()
-t2=Task2()
-t3=Task3()
-t1.start()
-t2.start()
-t3.start()
-
-
